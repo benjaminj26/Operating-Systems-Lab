7663c73e82ad4405df136dbc8a5cc0c88b35526f
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-08-09 14:42:45 +0530

    Completed Disk_Scheduling.c

--- C-Programs/Disk_Scheduling.c
+++ C-Programs/Disk_Scheduling.c
@@ -92,6 +92,160 @@ void sjf(int *positions, int len, int *current, int *total_distance)
 	}
 }
 
+void insertion_sort(int *array, int size)
+{
+	for(int i=1; i < size; ++i)
+	{
+		int key = array[i], j;
+		for(j=i-1; j >= 0 && key < array[j]; --j)
+		{
+			array[j+1] = array[j];
+		}
+		array[j+1] = key;
+	}
+}
+
+void scan(int *positions, int len, int *current, int *total_distance, int total_cylinders)
+{
+	*total_distance = 0;
+	int temp[len];
+	for(int i=0; i < len; ++i)
+	{
+		temp[i] = positions[i];
+	}
+	//Sorting the temp array of positions to make it easier for
+	//calculating the distance 
+	insertion_sort(temp, len);
+	if(*current <= temp[0])
+	{
+		*total_distance += temp[len - 1] - *current;
+	}
+	else
+	{
+		//Distance from current position to the end of the disk
+		*total_distance += total_cylinders - 1 - *current;
+		//Distance from end of the disk to the position at the other
+		//end of the disk
+		*total_distance += total_cylinders - 1 - temp[0];
+
+	}
+}
+
+void c_scan(int *positions, int len, int *current, int *total_distance, int total_cylinders)
+{
+	*total_distance = 0;
+	int temp[len];
+	for(int i=0; i < len; ++i)
+	{
+		temp[i] = positions[i];
+	}
+	insertion_sort(temp, len);
+	int starting_index;
+	if(*current > temp[len - 1])
+	{
+		*total_distance += total_cylinders - 1 - *current;
+		*total_distance += total_cylinders - 1;
+		*total_distance += temp[len - 1];
+	}
+	else if(*current == temp[len - 1])
+	{
+		*total_distance += total_cylinders - 1 - *current;
+		*total_distance += total_cylinders - 1;
+		*total_distance += temp[len - 2];
+	}
+	else if(*current <= temp[0])
+	{
+		*total_distance += temp[len - 1] - *current;
+	}
+	else
+	{
+		//To find the index of the nearest position to the current position
+		//which is before the current position
+		for(int i=0; i < len; ++i)
+		{
+			if(temp[i] > *current)
+			{
+				break;
+			}
+			else
+			{
+				//The index of the nearest postion to the current position 
+				//which is before the current position
+				starting_index = i;
+			}
+		}
+		//Distance between the current position and the end of the disk
+		*total_distance += total_cylinders - 1 - *current;
+		//Distance between the two ends of the disk
+		*total_distance += total_cylinders - 1;
+		//Distance from the beginning of the disk and the nearest position
+		//to the current position
+		*total_distance += temp[starting_index];
+	}
+}
+
+void look(int *positions, int len, int *current, int *total_distance, int total_cylinders)
+{
+	*total_distance = 0;
+	int temp[len];
+	for(int i=0; i < len; ++i)
+	{
+		temp[i] = positions[i];
+	}
+	insertion_sort(temp, len);
+	if(*current >= temp[len - 1])
+	{
+		*total_distance += *current - temp[0];
+	}
+	else if(*current <= temp[0])
+	{
+		*total_distance += temp[len - 1] - *current;
+	}
+	else
+	{
+		*total_distance += temp[len - 1] - *current;
+		*total_distance += temp[len - 1] - temp[0];
+	}
+}
+
+void c_look(int *positions, int len, int *current, int *total_distance, int total_cylinders)
+{
+	*total_distance = 0;
+	int temp[len];
+	for(int i=0; i < len; ++i)
+	{
+		temp[i] = positions[i];
+	}
+	insertion_sort(temp, len);
+	if(*current >= temp[len - 1])
+	{
+		*total_distance += *current - temp[0];
+		*total_distance += temp[len - 1] - temp[0];
+	}
+	else if(*current <= temp[0])
+	{
+		*total_distance += temp[len - 1] - *current;
+	}
+	else
+	{
+		int starting_index;
+		for(int i=0; i < len; ++i)
+		{
+			if(temp[i] > *current)
+			{
+				break;
+			}
+			else
+			{
+				starting_index = i;
+			}
+		}
+		*total_distance += temp[len - 1] - *current;
+		*total_distance += temp[len - 1] - temp[0];
+		*total_distance += temp[starting_index] - temp[0];
+	}
+}
+
 int main()
 {
 	printf("Enter the number of positions: ");
@@ -116,7 +270,11 @@ int main()
 		printf(
 			"1.FCFS\n"
 			"2.SJF\n"
-			"3.Exit\n"
+			"3.Scan\n"
+			"4.C Scan\n"
+			"5.Look\n"
+			"6.C Look\n"
+			"7.Exit\n"
 			"Enter your choice: "
 		);
 		int option;
@@ -126,7 +284,6 @@ int main()
 		{
 			case 1:
 			{
-				total_distance = 0;
 				current_position = initial_position;
 				fcfs(positions, len, &current_position, &total_distance);
 				printf("Total Distance: %d\n", total_distance);
@@ -135,14 +292,57 @@ int main()
 			
 			case 2:
 			{
-				total_distance = 0;
 				current_position = initial_position;
 				sjf(positions, len, &current_position, &total_distance);
 				printf("Total Distance: %d\n", total_distance);
 				break;
 			}
-			
+
 			case 3:
+			{
+				printf("Enter the total number of cylinders: ");
+				int total_cylinders;
+				scanf("%d", &total_cylinders);
+				current_position = initial_position;
+				scan(positions, len, &current_position, &total_distance, total_cylinders);
+				printf("Total Distabce: %d\n", total_distance);
+				break;
+			}
+
+			case 4:
+			{
+				printf("Enter the total number of cylinders: ");
+				int total_cylinders;
+				scanf("%d", &total_cylinders);
+				current_position = initial_position;
+				c_scan(positions, len, &current_position, &total_distance, total_cylinders);
+				printf("Total Distabce: %d\n", total_distance);
+				break;
+			}
+
+			case 5:
+			{
+				printf("Enter the total number of cylinders: ");
+				int total_cylinders;
+				scanf("%d", &total_cylinders);
+				current_position = initial_position;
+				look(positions, len, &current_position, &total_distance, total_cylinders);
+				printf("Total Distabce: %d\n", total_distance);
+				break;
+			}
+
+			case 6:
+			{
+				printf("Enter the total number of cylinders: ");
+				int total_cylinders;
+				scanf("%d", &total_cylinders);
+				current_position = initial_position;
+				c_look(positions, len, &current_position, &total_distance, total_cylinders);
+				printf("Total Distabce: %d\n", total_distance);
+				break;
+			}
+			
+			case 7:
 			{
 				exit_status = 0;
 				break;
@@ -153,6 +353,7 @@ int main()
 				printf("Invalid Input\n");
 			}
 		}
+		printf("\n");
 	}while(exit_status);
 	return 0;
 }
